dabfd21b0d7ae8a692ae4b8550cab82d4d9ca246
Kitchner Johnbull <86744479+Johnbullkitchner1@users.noreply.github.com>
2024-04-03 12:22:56 +0100

    Add files via upload

---
+++ projectcms/password.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectcms;
+
+/**
+ *
+ * @author Johnbull_101
+ */
+class password {
+
+    static String get;
+    
+}
